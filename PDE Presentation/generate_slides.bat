2d055a74b5d3b7f6b68f4ff7ee486079c4f2eea5
SouvikGhorui <sghorui29@gmail.com>
2026-03-31 14:57:08 +0530

    Generate high quality slides for presentation

--- PDE Presentation/generate_slides.bat
+++ PDE Presentation/generate_slides.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Generating slides (low Quality)...
-manim -ql energy_method_slides.py EnergyMethodPresentation
+echo Generating slides (High Quality 1080p)...
+manim -qh energy_method_slides.py EnergyMethodPresentation
 echo Done. You can now run present.bat to start presenting.
 pause
